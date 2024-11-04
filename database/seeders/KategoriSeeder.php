47e90fb70d68ac3d12456b2092a8ae41df4d6f64
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-11-04 18:53:31 +0200

    add new seeder

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class KategoriSeeder extends Seeder
 {
@@ -12,6 +13,47 @@ class KategoriSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('kategoris')->insert([
+            [
+                'nama' => 'Peralatan Elektronik',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Peralatan Kantor',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Buku dan Literatur',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Peralatan Olahraga',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Peralatan Kelas',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Furnitur',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Peralatan Laboratorium',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Alat Tulis Kantor',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
