a2fe3033134b323b60ae2d11846515eb65483002
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-11-06 21:34:09 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\RuangController;
 use App\Http\Controllers\TodoListController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 // Route::get('/dashboard', function () {
