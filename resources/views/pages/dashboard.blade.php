8da8020fef5d5f7bc0a3845cdfeff5046c89898b
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-11-06 22:51:16 +0200

    style(ui) update dashboard ui

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -118,7 +118,7 @@
 <div class="col-lg-12">
     <div class="card z-index-2">
         <div class="card-header pb-0">
-            <h6>Pekerjaan Selanjutnya</h6>
+            <h6>Tugas Selanjutnya</h6>
             @if (Session::has('status'))
             <div class="alert alert-success text-white opacity-5" role="alert">
                 {{ Session::get('message') }}
