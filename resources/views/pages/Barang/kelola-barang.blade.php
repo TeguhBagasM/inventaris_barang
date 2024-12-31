6027cb0638c7dd2df35e42e16cfca4b0048dfc91
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-12-31 09:51:11 +0200

    fix(datatables): fix error datatable issues

--- resources/views/pages/Barang/kelola-barang.blade.php
+++ resources/views/pages/Barang/kelola-barang.blade.php
@@ -21,7 +21,7 @@
                     </div>
                     <div class="card-body px-0 pt-0 pb-2">
                         <div class="table-responsive p-0">
-                            <table class="table align-items-center mb-0">
+                            <table class="table align-items-center mb-0" id="datatables">
                                 <thead>
                                     <tr>
                                         <th class="text-uppercase text-dark text-sm font-weight-bolder">No</th>
