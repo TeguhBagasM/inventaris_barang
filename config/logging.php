fa4a9f39f46c95a14118e9ad1a6e71af501ddcf1
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2025-01-06 21:57:03 +0200

    fix: logging.php configuration references

--- config/logging.php
+++ config/logging.php
@@ -84,6 +84,7 @@ return [
 
         'papertrail' => [
             'driver' => 'monolog',
+            'channels' => [],
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
             'handler_with' => [
